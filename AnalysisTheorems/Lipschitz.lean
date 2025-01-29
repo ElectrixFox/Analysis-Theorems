a5c124dc4248a31588e742465200746e826d76a0
ElectrixFox <69259529+ElectrixFox@users.noreply.github.com>
2025-01-29 19:31:07 +0000

    Have proved problem 1 Lipschitz

--- AnalysisTheorems/Lipschitz.lean
+++ AnalysisTheorems/Lipschitz.lean
@@ -13,11 +13,10 @@ lemma abs_nonneg_iff (a b : ℝ) (hab : 0 < a ∧ 0 < b) : 0 < |a - b| ↔ a ≠
     simp
     simp at h
     linarith
-    
-  
+
 example : ∀ (r : ℝ), r > 0 → lipschitz_cont r (fun x => 1 / x) := by
   intro r hr
-  use (1 / |r|)
+  use (1 / |r ^ 2|)
   constructor
   . positivity
   intro x y hxy
@@ -28,10 +27,24 @@ example : ∀ (r : ℝ), r > 0 → lipschitz_cont r (fun x => 1 / x) := by
   simp [hxy]
   push_neg at hxy
 
-  have hin : 1 / |x * y| ≤ 1 / |r| := by
+  have h1 : 0 < x := by linarith
+  have h2 : 0 < y := by linarith
+  have h3 : 0 < x * y := by positivity
+
+  have hin : 1 / |x * y| ≤ 1 / r ^ 2 := by
     rw [one_div_le]
     simp
-    rw [le_mul]
+
+    rw [abs_of_pos] <;> try linarith
+    
+    . 
+      rw [pow_two]
+      gcongr
+    . apply abs_pos_of_pos
+      simp_all
+    . simp_all
+    
+  rw [one_div_mul_eq_div]
   calc
     |1 / x - 1 / y| = |(y - x) / (x * y)| := by
       rw [sub_div]
@@ -41,13 +54,12 @@ example : ∀ (r : ℝ), r > 0 → lipschitz_cont r (fun x => 1 / x) := by
       linarith
     _ = |y - x| / |x * y| := by rw [abs_div]
     _ = |x - y| / |x * y| := by rw [abs_sub_comm]
-    _ ≤ |x - y| / |r| := by
+    _ ≤ |x - y| / r ^ 2 := by
       rw [←one_div_mul_eq_div, mul_comm]
-      rw [←one_div_mul_eq_div |r|, mul_comm (1 / |r|)]
+      rw [←one_div_mul_eq_div (r ^ 2), mul_comm (1 / r ^ 2)]
       rw [mul_le_mul_left]
       apply hin
       rw [abs_nonneg_iff]
       apply hxy
       constructor <;> linarith
-    
   
