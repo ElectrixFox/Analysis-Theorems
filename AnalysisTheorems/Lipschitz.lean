afc377db98240a796a3c6f7e8fe8f0b079459538
ElectrixFox <69259529+ElectrixFox@users.noreply.github.com>
2025-01-30 15:30:20 +0000

    Starting to try to implement IVT.

--- AnalysisTheorems/Lipschitz.lean
+++ AnalysisTheorems/Lipschitz.lean
@@ -2,6 +2,38 @@ import Mathlib
 
 def lipschitz_cont (r : ℝ) (f : ℝ → ℝ) : Prop := ∃ M > 0, ∀ (x y : ℝ), r < x ∧ r < y → |f x - f y| ≤ M * |x - y|
 
+def generalInterval [Preorder ℝ] (lower : Option ℝ) (upper : Option ℝ) : Set ℝ :=
+  { x | (lower.map (fun a => a ≤ x)).getD True ∧ (upper.map (fun b => x ≤ b)).getD True }
+
+def fun_point_limit (X : Set ℝ) (f : ℝ → ℝ) (c : ℝ) (L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x ∈ X \ {c}, |x - c| < δ → |f x - L| < ε
+
+def fun_right_limit₀ (X : Set ℝ) (f : ℝ → ℝ) (a L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x ∈ X, |x - a| < δ → |f x - L| < ε
+
+def fun_left_limit₀ (X : Set ℝ) (f : ℝ → ℝ) (b L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x ∈ X, |x - b| < δ → |f x - L| < ε
+
+def open_interval (a b : ℝ) : Set ℝ := {x : ℝ | a < x ∧ b < x}
+
+def fun_right_limit (X : open_interval a b) (f : ℝ → ℝ) (L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x ∈ (open_interval a b), |x - a| < δ → |f x - L| < ε
+
+def fun_left_limit (X : open_interval a b) (f : ℝ → ℝ) (L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x ∈ (open_interval a b), |x - b| < δ → |f x - L| < ε
+
+def fun_limit_bound_below (X : Set ℝ) (f : ℝ → ℝ) (L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ K > 0, ∀ x ∈ X, x ≥ K → |f x - L| < ε
+
+def fun_limit_bound_above (X : Set ℝ) (f : ℝ → ℝ) (L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ K > 0, ∀ x ∈ X, x ≤ K → |f x - L| < ε
+
+def continuous (f : ℝ → ℝ) (c : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x, |x - c| < δ → |f x - f c| < ε
+
+def continuous_on (f : ℝ → ℝ) (X : Set ℝ) : Prop :=
+  ∀ x, x ∈ X → continuous f x
+
 lemma abs_nonneg_iff (a b : ℝ) (hab : 0 < a ∧ 0 < b) : 0 < |a - b| ↔ a ≠ b := by
   constructor
   . contrapose
@@ -63,3 +95,12 @@ example : ∀ (r : ℝ), r > 0 → lipschitz_cont r (fun x => 1 / x) := by
       apply hxy
       constructor <;> linarith
   
+
+theorem intermediate_value_theorem (hX : X = generalInterval (some a) (some b)) (ha : a < b) (f : ℝ → ℝ) (hf : continuous_on f X) : (f b < f a ∧ d ∈ (generalInterval (f b) (f a))) ∨ (f a < f b ∧ d ∈ (generalInterval (f a) (f b))) → ∃ c ∈ X, f c = d := by
+  sorry
+
+example (hX : X = generalInterval (none) (none)): ∃ c, 1 < c ∧ c < 2 → (fun x => 2 * x ^ 3 - 3 * x ^ 2 + 7 * x - 9) c = 1 := by
+  simp_all
+  use 3 / 2
+  intro h1 h2
+  simp_all
