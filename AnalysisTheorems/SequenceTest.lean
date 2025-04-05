396cf6d4d8547a90207fdf541c74a3e0f2b4c372
ElectrixFox <lukecphillips2006@gmail.com>
2025-04-05 17:06:01 +0100

    Working on animations

--- AnalysisTheorems/SequenceTest.lean
+++ AnalysisTheorems/SequenceTest.lean
@@ -1,6 +1,13 @@
-import AnalysisTheorems.Sequences
+import Mathlib
+
+set_option trace.Elab.info true
+
+def seq_is_limit (x : ℕ → ℝ) (l : ℝ) : Prop :=
+  ∀ ε > 0, ∃ N, ∀ n ≥ N, |x n - l| < ε
 
 example : seq_is_limit (fun n => 6) 6 := by
   intro ε hε
   use 6
-  simp [hε]
+  intro n hn
+
+  rwa [show |_| < ε ↔ 0 < ε by ring_nf; rw [abs_zero]]
